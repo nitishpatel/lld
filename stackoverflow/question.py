792d5c1dcd3eef4de84a5f820c6312bb608f513c
Nitish Patel <nitishp74@gmail.com>
2025-06-22 23:54:56 +0530

    feat: question class init

--- stackoverflow/question.py
+++ stackoverflow/question.py
@@ -1,5 +1,13 @@
+from datetime import datetime
 class Question:
     '''implements the question class for stack overflow'''
 
-    def __init__(self):
-        pass
+    def __init__(self, author, title, content):
+        self.author = author
+        self.title = title
+        self.content = content
+        self.votes = 0
+        self.answers = []
+        self.comments = []
+        self.tags = []
+        self.created_at = datetime.now()
