d0e3599b5e970db0a72b211de589f104be62438b
Nitish Patel <nitishp74@gmail.com>
2025-06-22 23:47:34 +0530

    feat: implemented user class init

--- stackoverflow/stack_overflow_test.py
+++ stackoverflow/stack_overflow_test.py
@@ -3,4 +3,10 @@ from user import User
 
 def test_user_create():
     user = User(1,"Test_User","testuser@email.com",0)
-    assert user.id == 1
+    assert user.user_id == 1
+    assert user.username == "Test_User"
+    assert user.email == "testuser@email.com"
+    assert user.reputation == 0
+
+
+
--- stackoverflow/user.py
+++ stackoverflow/user.py
@@ -1,4 +1,9 @@
 
 class User:
+    '''Class to maintain user of stackoverflow'''
     def __init__(self,user_id:int,username:str,email:str,reputation:int)->None:
-        pass
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.reputation = reputation
+
