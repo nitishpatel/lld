ec0022a458829897071f232d9711685e5a76286f
Nitish Patel <nitishp74@gmail.com>
2025-06-22 23:53:47 +0530

    test: question create test

---
+++ stackoverflow/question.py
@@ -0,0 +1,5 @@
+class Question:
+    '''implements the question class for stack overflow'''
+
+    def __init__(self):
+        pass
--- stackoverflow/stack_overflow_test.py
+++ stackoverflow/stack_overflow_test.py
@@ -1,5 +1,10 @@
 import pytest
 from user import User
+from question import Question
+
+@pytest.fixture
+def user():
+    return User(1, "Test_User", "testuser@email.com", 0)
 
 def test_user_create():
     user = User(1,"Test_User","testuser@email.com",0)
@@ -7,6 +12,23 @@ def test_user_create():
     assert user.username == "Test_User"
     assert user.email == "testuser@email.com"
     assert user.reputation == 0
+
+def test_question_create(user):
+    question = Question(user,"What is Python?","help with python")
+    assert question.author == user
+    assert question.title == "What is Python?"
+    assert question.content == "help with python"
+    assert question.votes == 0
+    assert question.answers == []
+    assert question.comments == []
+    assert question.tags == []
+    assert question.created_at is not None
+
+
+
+
+
+
 
 
 
